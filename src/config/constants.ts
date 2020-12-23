4f2d206e985f19c010c885d4fe0401b8d9d92463
Guti <gustavo.costanti.lara@gmail.com>
2020-12-23 19:45:11 -0300

    Quarto commit

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,5 @@
 export const config = {
     API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION:
+        'mongodb+srv://Guti:r9gpwseb@url-shortener.udcih.mongodb.net/url-shortener?retryWrites=true&w=majority',
 }
