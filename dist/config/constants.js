e78de7e88970178c13df707ca4fb06988f84b736
Guti <gustavo.costanti.lara@gmail.com>
2021-01-02 13:20:26 -0300

    Fim do BackEnd

--- dist/config/constants.js
+++ dist/config/constants.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.config = void 0;
 exports.config = {
     API_URL: 'http://localhost:5000',
-    MONGO_CONNECTION: 'mongodb+srv://Guti:r9gpwseb@url-shortener.udcih.mongodb.net/url-shortener?retryWrites=true&w=majority',
+    MONGO_CONNECTION: 'mongodb+srv://user:encurtadorbrabo@url-shortener.udcih.mongodb.net/url-shortener?retryWrites=true&w=majority',
 };
 //# sourceMappingURL=constants.js.map
